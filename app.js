51f6ca5602c93b7c8e3a00770784ae6e1649886b
unknown <rushdy.naloordeen@arimaclanka.com>
2024-02-19 13:30:19 +0530

    task-NNP-2024-02-19

--- app.js
+++ app.js
@@ -65,8 +65,162 @@ router.post('/item', (req,res)=> {
     }
 });
 
-router.get('/item', (req,res)=> {
+router.get('/item-retrive', (req,res)=> {
+
+    try {
+        const sql = "SELECT * FROM items_table";
+        con.query(sql, (error, result)=> { 
+            if (error) throw error;
+            console.log(result);
+            return res.json({
+                status: true,
+                code: 200,
+                data: result
+            }); 
+            
+        });
+
+    } catch (error) {
+        return res.json({
+            status: false,
+            code: 500,
+            data: error
+        }); 
+    }
+});
+
+router.put('/edit/item/:id', (req,res)=> {
     
+    try {
+
+        const ids = req.params.id;
+        const item_name = req.body.item_name;
+        const item_price = req.body.item_price;
+        const item_availability = req.body.item_availability;
+        const item_quantity = req.body.item_quantity;
+
+        let msg = '';
+        if (!item_name) 
+        {
+           return res.json(msg = 'item name is required');
+        }
+        if (!item_price)
+        {
+           return res.json(msg = 'item price is required');
+        }
+        if (!item_availability)
+        {
+           return res.json(msg = 'item availability is required');
+        }
+        if (typeof item_availability !== 'boolean')
+        {
+           return res.json(msg = 'It is not a boolean value');
+        }
+        if (!item_quantity)
+        {
+           return res.json(msg = 'item_quantity is required');
+        }
+
+            const sql1 = "SELECT item_id FROM items_table";
+            con.query(sql1, (error, results) => {
+             
+              if (error) throw error;
+              console.log(results);
+             
+            const itemIds = results.map(result => result.item_id);
+            console.log(itemIds);
+            console.log(req.params.id);
+            
+              if (req.params.id) {
+
+              }
+             
+              else {
+
+            const sql = "UPDATE items_table SET name = ?, price = ?, availability = ?, quantity = ? WHERE item_id = ?";
+            con.query(sql,[item_name,item_price,item_availability,item_quantity,ids], (error,result)=> {
+            if (error) throw error; 
+
+            });
+                console.log('Ok');
+                
+                return res.json({
+                    status: true,
+                    code: 200,
+                    data: `${ids} Is Updated Succesfully`
+                }); 
+              }
+    
+            });
+
+        
+    } catch (error) {
+        return res.json({
+            status: false,
+            code: 500,
+            data: error
+        }); 
+    }
+  
+
+});
+
+
+router.get('/test/:id', (req,res)=> {
+
+    const id = req.params.id;
+    const sql1 = "SELECT item_id FROM items_table";
+
+    con.query(sql1, [], (error, results) => {
+      if (error) {
+        throw error;
+      }
+    
+      if (results[0].item_id == id) {
+        console.log('Ok');
+        return res.json({
+            status: true,
+            code: 200,
+            data: `Done`
+        }); 
+      }
+      else {
+        console.log('NotOk');
+        return res.json({
+            status: true,
+            code: 200,
+            data: `${id} Not Found`
+        }); 
+      }
+      console.log(results);
+    
+    });
+});
+
+router.delete('/remove/item/:id', (req,res)=> {
+    
+    try {
+
+        const id  = req.params.id;
+        const sql = "DELETE FROM items_table WHERE item_id = ?";
+
+        con.query(sql,[id], (error,result)=> {
+           if (error) throw error;
+           return res.json({
+            status: false,
+            code: 500,
+            data: `${id} Is Deleted Successfully` 
+           }); 
+        });
+        
+        
+    } catch (error) {
+        return res.json({
+            status: false,
+            code: 500,
+            data: error
+        }); 
+    }
 
 });
 
